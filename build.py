33fca91e08a466d86d498991e3e625cdbe1a03d4
Sixten Hilborn <you@example.com>
2018-04-12 07:29:22 +0200

    [CI] Build shared MT

--- build.py
+++ build.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 
-from bincrafters import build_template_default
+from bincrafters import build_shared
 
 if __name__ == "__main__":
-
-    builder = build_template_default.get_builder()
+    builder = build_shared.get_builder()
+    builder.add_common_builds(shared_option_name="libmikmod:shared", pure_c=True, dll_with_static_runtime=True)
 
     builder.run()
     
