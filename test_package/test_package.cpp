512a73fa550de62e53796dfa5c8d72606774daa9
Sixten Hilborn <sixten.hilborn@gmail.com>
2019-01-03 16:54:21 +0100

    Add some basic MikMod calls in test_package.cpp

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,5 +1,17 @@
 #include <mikmod.h>
+#include <iostream>
 
 int main()
 {
+    // register all the drivers
+    MikMod_RegisterAllDrivers();
+
+    // initialize the library
+    if (int err = MikMod_Init("")) {
+        std::cerr << "Couldn't initialize sound, reason: " << MikMod_strerror(MikMod_errno) << "\n";
+        return err;
+    }
+
+    // cleanup
+    MikMod_Exit();
 }
