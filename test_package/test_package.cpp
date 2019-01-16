8f514926dd6ab421e00e1a7ecd1dedb51229e355
Sixten Hilborn <sixten.hilborn@gmail.com>
2019-01-16 11:42:16 +0100

    Ignore test errors on macos in travis

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,4 +1,6 @@
 #include <mikmod.h>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
 
 int main()
@@ -9,7 +11,12 @@ int main()
     // initialize the library
     if (int err = MikMod_Init("")) {
         std::cerr << "Couldn't initialize sound, reason: " << MikMod_strerror(MikMod_errno) << "\n";
-        return err;
+        // some CI environments doesn't support audio, so we'll just ignore errors there
+        const char* ignoreEnvvar = std::getenv("IGNORE_LIBMIKMOD_TEST_ERRORS");
+        if (ignoreEnvvar && std::strcmp(ignoreEnvvar, "true") == 0)
+            return 0
+        else
+            return err;
     }
 
     // cleanup
